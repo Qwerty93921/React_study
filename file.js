069a929e36d2cd4e8bd84cef12c72bec99855fca
Qwerty93921 <134071623+Qwerty93921@users.noreply.github.com>
2024-10-15 17:17:34 +0600

    file 1

---
+++ file.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// DOM - Document Object Model
+// SPA - Single Page Application
+// https://react.dev/learn
+
+//npx create-react-app my-react-app
+// После npx create-react-app идет название приложения
+
+// Если команда не работает, нужно папку "npm" создать вручную
+// C:\Users\Me\AppData\Roaming\npm
+
+// npm start
+// Запускает страницу на localhost:3000
